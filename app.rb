e5a4aa714884039cb74f2ff70b4edbc32079a555
enodal <164991691+enodal@users.noreply.github.com>
2024-04-16 17:35:58 +0000

    start

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  "hello there"
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
